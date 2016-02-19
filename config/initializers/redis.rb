4ad34dfe5a955f79062df194a62acc15bb24916f
hechen0 <me@hechen0.com>
2016-02-20 01:58:16 +0800

    set redis current

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
-  require 'redis-classy'
-  RedisClassy.redis = Redis.new(
+  Redis.current = Redis.new(
     host: ENV["REDIS_PORT_6379_TCP_ADDR"],
     port: ENV["REDIS_PORT_6379_TCP_PORT"],
     password: ENV["REDIS_PASSWORD"]

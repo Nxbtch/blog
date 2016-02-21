083057b7e2b6a906662177b7bf3a500adcdfcaa7
hechen0 <me@hechen0.com>
2016-02-21 21:42:44 +0800

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.4.0'
 
 set :application, 'app'
-set :repo_url, 'git@hechen0.me:me/my_repo.git'
+set :repo_url, 'git@gitcafe.com:hechen0/app.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/h/app'
 
 # Default value for :scm is :git
 # set :scm, :git
